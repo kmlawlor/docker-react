79ab0b4e82644c63a65580bfdc23b825697aec69
kmlawlor <kmlawlor@gmail.com>
2021-04-20 10:44:07 -0400

    initial commit

---
+++ App.test.js
@@ -0,0 +1,14 @@
+import { findAllByTestId, render, screen } from '@testing-library/react';
+import App from './App';
+
+findAllByTestId('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+it('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
